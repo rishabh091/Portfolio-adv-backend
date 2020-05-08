3b9ba4baab000e9d1c526a508c2f2710679e7a93
Rishabh <rishabhmalhotra091@gmail.com>
2020-05-08 15:38:40 +0530

    message system completed

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const mail = require('./mail.js');
+const bodyParser = require('body-parser');
+
+const json = bodyParser.json();
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+app.post('/mail', json, (req, res) => {
+    res.send(mail.sendMessage(req.body));
+})
+
 const server = app.listen(process.env.PORT, (err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log('Server running at port : ' + server.address().port);
 })
---
+++ mail.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var nodemail = require('nodemailer');
+
+const Transport = nodemail.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'rishabhmalhotra9211@gmail.com',
+        pass: "SyncMasterB1930"
+    }
+})
+
+let mail = {
+    from: 'rishabhmalhotra9211@gmail.com',
+    to: 'rishabhmalhotra091@gmail.com',
+    subject: 'Message from {name}',
+    text: ''
+}
+
+function sendMessage(body) {
+    console.log("Message from : " + body.name);
+    mail.subject = mail.subject.replace('{name}', body.name);
+
+    //read data from file
+    fs.readFile('statics/format.txt', (err, data) => {
+        if (err) throw err;
+        mail.text = data.toString();
+        mail.text = mail.text.replace('{message}', body.message)
+            .replace('{email}', body.email);
+
+        Transport.sendMail(mail, (err, info) => {
+            if (err) throw err;
+            console.log('Message sent ! Value : ' + info.toString());
+        })
+    })
+}
+
+module.exports = {
+    sendMessage: sendMessage
+}
